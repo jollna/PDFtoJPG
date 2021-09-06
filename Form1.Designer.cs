6cb4c0540de278226f5e8a25de71c6299254924a
jollan <jollna@outlook.com>
2021-09-06 17:38:03 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace PDFtoJPG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPDF = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtJPG = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPDF
+            // 
+            this.txtPDF.Location = new System.Drawing.Point(96, 11);
+            this.txtPDF.Name = "txtPDF";
+            this.txtPDF.Size = new System.Drawing.Size(180, 21);
+            this.txtPDF.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PDF文件路径:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "JPG保存路径:";
+            // 
+            // txtJPG
+            // 
+            this.txtJPG.Location = new System.Drawing.Point(96, 46);
+            this.txtJPG.Name = "txtJPG";
+            this.txtJPG.ReadOnly = true;
+            this.txtJPG.Size = new System.Drawing.Size(180, 21);
+            this.txtJPG.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(140, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "开始转换";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.comboBox1.Location = new System.Drawing.Point(96, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(38, 20);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.Text = "1";
+            this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "图片缩放倍率:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(95, 110);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(182, 14);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "转换进度:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(212, 77);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "打开目录";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 136);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtJPG);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPDF);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 175);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 175);
+            this.Name = "Form1";
+            this.Text = "PDF转JPG";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPDF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtJPG;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Win32;
+using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFtoJPG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtPDF.Clear();
+            List<string> fileList;
+            fileList = GetFiles("请选择需要转换的PDF文件", ".pdf", "PDF文件|*.pdf;*PDF");
+            if (fileList == null)
+            {
+                txtPDF.Text = "";
+                return;
+            }
+            foreach (var item in fileList)
+            {
+                txtPDF.Text += item;
+                DirectoryInfo pathInfo = new DirectoryInfo(item);
+                txtJPG.Text = pathInfo.Parent.FullName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folder = new System.Windows.Forms.FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                //SelectedPath:获取文件夹绝对路径,显示在textbox里面
+                this.txtJPG.Text = folder.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string name = null;
+            if (!File.Exists(txtPDF.Text))
+            {
+                MessageBox.Show("输入的PDF文件不存在，请重新选择");
+                return;
+            }
+            if (!Directory.Exists(txtJPG.Text))
+            {
+                MessageBox.Show("选择的保存位置不存在，请重新选择");
+                return;
+            }
+            if (comboBox1.Text == "")
+            {
+                comboBox1.Text = 1.ToString();
+            }
+            string strpdfPath = txtPDF.Text.ToString();
+            var pdf = PdfDocument.Load(strpdfPath);
+            var pdfpage = pdf.PageCount;
+            var pagesizes = pdf.PageSizes;
+
+            //设置进度条信息
+            progressBar1.Value = 0;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = pdfpage;
+
+            for (int i = 1; i <= pdfpage; i++)
+            {
+                Size size = new Size();
+                progressBar1.Value++;
+                Thread.Sleep(500);
+                int PDFWidth = (int)(pagesizes[(i - 1)].Width * 1.5);
+                int PDFHeight = (int)(pagesizes[(i - 1)].Height * 1.5);
+                size.Height = PDFHeight * int.Parse(comboBox1.Text);
+                size.Width = PDFWidth * int.Parse(comboBox1.Text);
+                if (size.Width * size.Height > 250000000)
+                {
+                    size = Getsize(PDFWidth, PDFHeight, int.Parse(comboBox1.Text));
+                    name = name + i.ToString() + "、";
+                }
+                RenderPage(strpdfPath, i, size, txtJPG.Text.ToString() + "\\" + Path.GetFileNameWithoutExtension(strpdfPath) + i + @".jpg");
+            }
+            if (name != null)
+            {
+                MessageBox.Show("转换成功！\n第" + name + "张图分辨率过大，已自动调整为适合输出的最大倍率。");
+            }
+            else
+            {
+                MessageBox.Show("转换成功！");
+            }
+        }
+
+        public void RenderPage(string pdfPath, int pageNumber, System.Drawing.Size size, string outputPath, int dpi = 300)
+        {
+            using (var document = PdfiumViewer.PdfDocument.Load(pdfPath))
+            using (var stream = new FileStream(outputPath, FileMode.Create))
+            using (var image = GetPageImage(pageNumber, size, document, dpi))
+            {
+                image.Save(stream, ImageFormat.Jpeg);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pageNumber">pdf文件张数</param>
+        /// <param name="size">pdf文件尺寸</param>
+        /// <param name="document">pdf文件位置</param>
+        /// <param name="dpi"></param>
+        /// <returns></returns>
+        private static System.Drawing.Image GetPageImage(int pageNumber, Size size, PdfiumViewer.PdfDocument document, int dpi)
+        {
+            return document.Render(pageNumber - 1, size.Width, size.Height, dpi, dpi, PdfRenderFlags.Annotations);
+        }
+
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(e.KeyChar == '\b' || (e.KeyChar >= '0' && e.KeyChar <= '9')))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// 调用系统资源管理器，打开文件夹选择文件
+        /// </summary>
+        /// <param name="Title">文件对话框标题</param>
+        /// <param name="DefaultExt">设置默认文件扩展名</param>
+        /// <param name="Filter">设置当前文件名筛选器字符串，该字符串决定对话框的“另存为文件类型”或“文件类型”框中出现的选择内容。</param>
+        /// <returns></returns>
+        public static List<string> GetFiles(string Title, string DefaultExt, string Filter)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = Title;
+            dlg.Multiselect = false;//等于true表示可以选择多个文件
+            dlg.DefaultExt = DefaultExt;
+            dlg.Filter = Filter;
+            var fileList = new List<string>();
+            if (dlg.ShowDialog() != DialogResult.OK)
+            {
+                return null;
+            }
+            foreach (string file in dlg.FileNames)
+            {
+                fileList.Add(file);
+            }
+            return fileList;
+        }
+
+        public static Size Getsize(int Width, int Height, int multiple)
+        {
+            Size size = new Size();
+            size.Width = Width * multiple;
+            size.Height = Height * multiple;
+            int i = size.Height * size.Width;
+            if (size.Height * size.Width < 250000000)
+                return size;
+            return Getsize(Width, Height, multiple - 1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start(txtJPG.Text);
+        }
+    }
+}
